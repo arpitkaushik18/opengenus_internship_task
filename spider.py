22c4b7eb9a2f7ea8426d1a3d6fb44b0839d4b632
arpitkaushik18 <30577073+arpitkaushik18@users.noreply.github.com>
2018-12-10 13:07:56 +0530

    Update spider.py

--- spider.py
+++ spider.py
@@ -19,7 +19,7 @@ class Spider:
         Spider.queue_file = Spider.project_name + '/queue.txt'
         Spider.crawled_file = Spider.project_name + '/crawled.txt'
         self.boot()
-        self.crawl_page('First spider',Spider.base_url)
+        self.crawl_page(Spider.base_url)
 
     @staticmethod
     def boot():
